b84f49e46ea07034cdde00b4d689a707049aed26
Jay Qi <jayqi@users.noreply.github.com>
2024-06-02 20:15:40 -0400

    Add caching

--- chrome-extension/main.js
+++ chrome-extension/main.js
@@ -1,41 +1,34 @@
-let message;
+const hostName = 'com.jayqi.linkedin_notes';
 let port = null;
+let profile = null;
 
-function appendMessage(text) {
-    // document.getElementById('response').innerHTML += '<p>' + text + '</p>';
-}
-
-function updateUiState() {
-    if (port) {
-        // document.getElementById('connect-button').style.display = 'none';
-        // document.getElementById('input-text').style.display = 'block';
-        // document.getElementById('send-message-button').style.display = 'block';
-    } else {
-        // document.getElementById('connect-button').style.display = 'block';
-        // document.getElementById('input-text').style.display = 'none';
-        // document.getElementById('send-message-button').style.display = 'none';
-    }
-}
-
-function onNativeMessage(message) {
-    appendMessage('Received message: <b>' + JSON.stringify(message) + '</b>');
+async function onNativeMessage(message) {
     if (message.mode == 'read') {
-        if (message.payload.text !== null) {
+        document.getElementById('notes-field').placeholder = 'Enter a note here.';
+        if (message.payload.text) {
             console.log("Successfully retrieved note from database.");
             document.getElementById('notes-field').value = message.payload.text;
         } else {
             console.log("Note is blank.");
-            document.getElementById('notes-field').placeholder = 'Enter a note here.';
         }
-        document.getElementById('notes-field').removeAttribute('disabled');
         document.getElementById('save-notes-button').setAttribute('disabled', '');
         document.getElementById('reset-notes-button').setAttribute('disabled', '');
+        // Check cache for unsaved changes
+        const cached_text = await readCache();
+        if (cached_text) {
+            document.getElementById('notes-field').value = cached_text;
+            document.getElementById('notes-status').innerText = 'You have unsaved changes.';
+            document.getElementById('save-notes-button').removeAttribute('disabled');
+            document.getElementById('reset-notes-button').removeAttribute('disabled');
+        }
+        document.getElementById('notes-field').removeAttribute('disabled');
     } else if (message.mode == 'write') {
         if (message.payload.success) {
             console.log("Successfully saved note to database.");
             document.getElementById('notes-status').innerText = 'Changes saved.';
             document.getElementById('save-notes-button').setAttribute('disabled', '');
             document.getElementById('reset-notes-button').setAttribute('disabled', '');
+            await removeCache();
         } else {
             console.log("Failed to save note to database.");
             document.getElementById('notes-status').innerText = 'Changes failed to save.';
@@ -45,25 +38,8 @@ function onNativeMessage(message) {
     }
 }
 
-function onNotesFieldChange() {
-    document.getElementById('notes-status').innerText = 'You have unsaved changes.';
-    document.getElementById('save-notes-button').removeAttribute('disabled');
-    document.getElementById('reset-notes-button').removeAttribute('disabled');
-}
-
 function onDisconnected() {
-    appendMessage('Failed to connect: ' + chrome.runtime.lastError.message);
     port = null;
-    updateUiState();
-}
-
-function connect() {
-    const hostName = 'com.jayqi.linkedin_notes';
-    appendMessage('Connecting to native messaging host <b>' + hostName + '</b>');
-    port = chrome.runtime.connectNative(hostName);
-    port.onMessage.addListener(onNativeMessage);
-    port.onDisconnect.addListener(onDisconnected);
-    updateUiState();
 }
 
 async function getActiveTabUrl() {
@@ -72,48 +48,83 @@ async function getActiveTabUrl() {
     return new URL(tab.url);
 }
 
-function extractProfile(pathname) {
+function setProfile(pathname) {
     const regex = /^\/in\/[^\/]+\//;
     const match = pathname.match(regex);
-    return match ? match[0] : null;
+    profile = match ? match[0] : null;
 }
 
-function queryDatabase(profile) {
+function queryDatabase() {
     message = { profile: profile, mode: 'read' };
     port.postMessage(message);
-    appendMessage('Sent message: <b>' + JSON.stringify(message) + '</b>');
 }
 
-function saveToDatabase(profile) {
+function saveToDatabase() {
     message = { profile: profile, text: document.getElementById('notes-field').value, mode: 'write' };
     port.postMessage(message);
-    appendMessage('Sent message: <b>' + JSON.stringify(message) + '</b>');
 }
 
-function discardChanges(profile) {
-    queryDatabase(profile);
-    document.getElementById('notes-status').innerText = '\u00A0';
+async function writeCache(text) {
+    console.log("Writing cache: ", profile);
+    await chrome.storage.local.set({[profile]: text});
+    console.log("Cache written.")
+}
+
+async function readCache() {
+    console.log("Reading from cache: ", profile)
+    const result = await chrome.storage.local.get([profile]);
+    console.log("Cache read: ", result);
+    if (profile in result) {
+        return result[profile];
+    } else {
+        return null;
+    }
+}
+
+async function removeCache() {
+    console.log("Removing cache for: ", profile);
+    await chrome.storage.local.remove([profile]);
+    console.log("Cache removed.");
+}
+
+async function onNotesFieldChange() {
+    document.getElementById('notes-status').innerText = 'You have unsaved changes.';
+    document.getElementById('save-notes-button').removeAttribute('disabled');
+    document.getElementById('reset-notes-button').removeAttribute('disabled');
+    await writeCache(document.getElementById('notes-field').value);
+}
+
+async function discardChanges() {
+    // Query database to reset notes
+    queryDatabase();
+    document.getElementById('notes-status').innerText = 'Changes discarded.';
+    await removeCache();
 }
 
 document.addEventListener('DOMContentLoaded', async function () {
     console.log("Running DOMContentLoaded hook");
+
     const url = await getActiveTabUrl();
     console.log(url);
     if (url.hostname === 'www.linkedin.com' && url.pathname.startsWith('/in/')) {
-        console.log("URL is LinkedIn profile.");
-        const profile = extractProfile(url.pathname);
+        console.log("Active tab is a LinkedIn profile.");
+        setProfile(url.pathname);
         console.log("Profile: ", profile);
+
         document.getElementById('not-linkedin-container').style.display = 'none';
-        connect();
         document.getElementById('is-linkedin-container').style.display = 'block';
 
-        // Query database storage for note for this profile
+        // Connect to native messaging host
+        port = chrome.runtime.connectNative(hostName);
+        port.onMessage.addListener(onNativeMessage);
+        port.onDisconnect.addListener(onDisconnected);
+
+        // Query database storage for notes for this profile
         queryDatabase(profile);
 
-        document.getElementById('save-notes-button').addEventListener('click', function() {saveToDatabase(profile)});
-        document.getElementById('reset-notes-button').addEventListener('click', function() {discardChanges(profile)});
+        // Register event listeners
+        document.getElementById('save-notes-button').addEventListener('click', saveToDatabase);
+        document.getElementById('reset-notes-button').addEventListener('click', discardChanges);
         document.getElementById('notes-field').addEventListener('input', onNotesFieldChange);
-
-        updateUiState();
     }
 });
